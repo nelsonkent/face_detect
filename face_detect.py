3e5dd7feec76517e47e2fa5d6b432be2a1125b3b
Nelson <huangwang@live.cn>
2017-02-16 18:30:49 +0800

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,14 +1,18 @@
 import cv2
 import sys
 import string
+import  PIL.Image
+from io import StringIO 
 
-def handerImage(imagePath, cascPath):
+def handerImage(stream, cascPath):
     
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
 
     # Read the image
-    image = cv2.imread(imagePath)
+    ##image = cv2.imread(imagePath)
+    temp = StringIO(stream)
+    img = PIL.Image.open(temp)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the image
@@ -25,23 +29,15 @@ def handerImage(imagePath, cascPath):
     for (x, y, w, h) in faces:
         crop = image[y:y+h,x:x+w]
         count = count + 1
-        ##存储
-        import time
-        filePath = "C:\\Users\\nelson\\Desktop\\face_detect\\happy\\"
-        now = time.time()
-        fileName = str(now) 
-        filePath = filePath + fileName + ".png"
-        print(filePath)
-        cv2.imwrite(filePath, crop)
-        ##cv2.imshow('crop'+str(count+1), crop)
-    print(count)   
+        cv2.imshow('crop'+str(count+1), crop)
+        print(count)
+        
 
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
     cv2.imshow("Faces found", image)
     cv2.waitKey(0)
 
-handerImage('abba.png' ,'haarcascade_frontalface_default.xml')
 
 ### Draw a rectangle around the faces
 
