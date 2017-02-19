c0790b78c0a6797cdf8b3e20003eb7062288fd47
Nelson <huangwang@live.cn>
2017-02-19 23:29:09 +0800

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -3,41 +3,47 @@ import sys
 import string
 import  PIL.Image
 from io import StringIO 
+import os
+import time
 
-def handerImage(stream, cascPath):
-    
-    # Create the haar cascade
-    faceCascade = cv2.CascadeClassifier(cascPath)
-
-    # Read the image
-    ##image = cv2.imread(imagePath)
-    temp = StringIO(stream)
-    img = PIL.Image.open(temp)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the image
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-
-    print("Found {0} faces!".format(len(faces)))
-    count = 0
-    for (x, y, w, h) in faces:
-        crop = image[y:y+h,x:x+w]
-        count = count + 1
-        cv2.imshow('crop'+str(count+1), crop)
-        print(count)
+class ImageHander(object):
+    def __init__(self, cascPath):
+        self.faceCascade = cv2.CascadeClassifier(cascPath)
         
+    def handerImage(self, filename, dirPath, imgType):
+        # Read the image
+        ##image = cv2.imread(imagePath)
+        #temp = StringIO(stream)
+        #img = PIL.Image.open(temp)
+        image = cv2.imread(filename)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Detect faces in the image
+        faces = self.faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.imshow("Faces found", image)
-    cv2.waitKey(0)
+        print("Found {0} faces!".format(len(faces)))
+        count = 0
+        for (x, y, w, h) in faces:
+            crop = image[y:y+h,x:x+w]
+            count = count + 1
+            dirPath = os.path.join(sys.path[0], dirPath)
+            name = time.strftime("%Y%m%d%H%M%S", time.localtime())
+            newfile = os.path.join(dirPath,  name + str(count) + "." + imgType)
+            print(newfile)
+            cv2.imwrite(newfile, crop)
+            #cv2.imshow('crop'+str(count+1), crop)
+            #print('face found  %s' % count)
+            
+
+        #for (x, y, w, h) in faces:
+        #   cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #cv2.imshow("Faces found", image)
+        #cv2.waitKey(0)
 
 
 ### Draw a rectangle around the faces
-
