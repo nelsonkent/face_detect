a2a0f3c9662973fba2e48452bb6593badacb397a
Nelson <huangwang@live.cn>
2017-02-23 19:17:49 +0800

    Add files via upload

--- face_detect.py
+++ face_detect.py
@@ -2,21 +2,26 @@ import cv2
 import sys
 import string
 import  PIL.Image
-from io import StringIO 
+import io
+import requests
 import os
 import time
+import numpy
+count = 0
 
 class ImageHander(object):
     def __init__(self, cascPath):
         self.faceCascade = cv2.CascadeClassifier(cascPath)
         
-    def handerImage(self, filename, dirPath, imgType):
-        # Read the image
-        ##image = cv2.imread(imagePath)
-        #temp = StringIO(stream)
-        #img = PIL.Image.open(temp)
-        image = cv2.imread(filename)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def handerImage(self, url, dirPath, imgType):
+        print(url)
+        try:
+            file = io.BytesIO(requests.get(url, timeout=10).content)
+            image = PIL.Image.open(file)
+            image = numpy.array(image)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        except Exception as e:
+            return
         # Detect faces in the image
         faces = self.faceCascade.detectMultiScale(
             gray,
@@ -27,23 +32,22 @@ class ImageHander(object):
         )
 
         print("Found {0} faces!".format(len(faces)))
-        count = 0
+        global count 
         for (x, y, w, h) in faces:
             crop = image[y:y+h,x:x+w]
             count = count + 1
-            dirPath = os.path.join(sys.path[0], dirPath)
             name = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            newfile = os.path.join(dirPath,  name + str(count) + "." + imgType)
+            newfile = os.path.join(dirPath,  name + str(count)+'.'+ imgType)
             print(newfile)
             cv2.imwrite(newfile, crop)
-            #cv2.imshow('crop'+str(count+1), crop)
-            #print('face found  %s' % count)
-            
 
-        #for (x, y, w, h) in faces:
-        #   cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        #cv2.imshow("Faces found", image)
-        #cv2.waitKey(0)
+            if __name__ == '__main__':
+                for (x, y, w, h) in faces:
+                   cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.imshow("Faces found", image)
+                cv2.waitKey(0)
 
-
+if __name__ == '__main__':
+    imageHander = ImageHander('haarcascade_frontalface_default.xml')
+    imageHander.handerImage('http://img.ishuo.cn/1609/1474689789.jpg', 'happy', 'jpg')
 ### Draw a rectangle around the faces
